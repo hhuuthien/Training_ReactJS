35a139adab9ab5e4900062027575763647769c33
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-05-09 23:47:16 +0700

    Buoi 13

--- src/pages/Thunk/ToDoListApp.jsx
+++ src/pages/Thunk/ToDoListApp.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getTasksAPIAction } from "../../redux/actions/toDoListAction";
+import { addTaskAPIAction, doneTask, getTasksAPIAction, rejectTask, xoaTask } from "../../redux/actions/toDoListAction";
 
 export default function ToDoListApp() {
   const { tasks } = useSelector((root) => root.toDoListReducer);
+
+  const taskRef = useRef({ taskName: "" });
 
   const dispatch = useDispatch();
 
@@ -14,26 +16,98 @@ export default function ToDoListApp() {
     dispatch(action);
   }, []);
 
+  const handleChange = (event) => {
+    let { id, value } = event.target;
+    taskRef.current[id] = value;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // post lên
+      const actiontion = addTaskAPIAction(taskRef.current);
+      dispatch(actiontion);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const handleDelete = (taskName) => {
+    const action = xoaTask(taskName);
+    dispatch(action);
+  };
+
+  const handleDone = (taskName) => {
+    const action = doneTask(taskName);
+    dispatch(action);
+  };
+
+  const handleReject = (taskName) => {
+    const action = rejectTask(taskName);
+    dispatch(action);
+  };
+
   return (
     <div className="container p-5">
       <h4 className="mb-5">To do list app</h4>
 
+      <form className="input-group mb-5" onSubmit={handleSubmit}>
+        <input type="text" className="form-control" id="taskName" name="taskName" onChange={handleChange} />
+        <button className="btn btn-success" type="submit">
+          Thêm
+        </button>
+      </form>
+
       <table className="table w-50">
         <tbody>
-          {tasks.map((task, index) => {
-            return (
-              <tr key={index}>
-                <td>{task.taskName}</td>
-                <td>
-                  <span className="badge badge-warning">In progress</span>
-                </td>
-                <td>
-                  <button className="btn btn-success">Xong</button>
-                </td>
-              </tr>
-            );
-          })}
+          {tasks
+            .filter((t) => t.status !== true)
+            .map((task, index) => {
+              return (
+                <tr key={index}>
+                  <td>{task.taskName}</td>
+                  <td>
+                    <span className="badge badge-warning">In progress</span>
+                  </td>
+                  <td>
+                    <button className="btn btn-success" onClick={() => handleDone(task.taskName)}>
+                      Xong
+                    </button>
+                  </td>
+                  <td>
+                    <button className="btn btn-danger" onClick={() => handleDelete(task.taskName)}>
+                      Xoá
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
+        <tfoot>
+          {tasks
+            .filter((t) => t.status === true)
+            .map((task, index) => {
+              return (
+                <tr key={index}>
+                  <td>{task.taskName}</td>
+                  <td>
+                    <span className="badge badge-success">Completed</span>
+                  </td>
+                  <td>
+                    <button className="btn btn-warning" onClick={() => handleReject(task.taskName)}>
+                      Undo
+                    </button>
+                  </td>
+                  <td>
+                    <button className="btn btn-danger" onClick={() => handleDelete(task.taskName)}>
+                      Xoá
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tfoot>
       </table>
     </div>
   );
--- src/redux/actions/toDoListAction.js
+++ src/redux/actions/toDoListAction.js
@@ -15,3 +15,72 @@ export const getTasksAPIAction = async (dispatch2) => {
     console.log(e);
   }
 };
+
+export const addTaskAPIAction = (taskData) => {
+  return async (dispatch2) => {
+    try {
+      // post lên
+      const result = await axios({
+        url: "http://svcy.myclass.vn/api/ToDoList/AddTask",
+        method: "POST",
+        data: taskData,
+      });
+
+      const action = getTasksAPIAction;
+      dispatch2(action);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+};
+
+export const xoaTask = (taskName) => {
+  return async (dispatch) => {
+    try {
+      // xoá
+      const result = await axios({
+        url: "http://svcy.myclass.vn/api/ToDoList/deleteTask?taskName=" + taskName,
+        method: "DELETE",
+      });
+
+      alert(result.data);
+
+      const action = getTasksAPIAction;
+      dispatch(action);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+};
+
+export const doneTask = (taskName) => {
+  return async (dispatch) => {
+    try {
+      const result = await axios({
+        url: "http://svcy.myclass.vn/api/ToDoList/doneTask?taskName=" + taskName,
+        method: "PUT",
+      });
+
+      const action = getTasksAPIAction;
+      dispatch(action);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+};
+
+export const rejectTask = (taskName) => {
+  return async (dispatch) => {
+    try {
+      const result = await axios({
+        url: "http://svcy.myclass.vn/api/ToDoList/rejectTask?taskName=" + taskName,
+        method: "PUT",
+      });
+
+      const action = getTasksAPIAction;
+      dispatch(action);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+};
